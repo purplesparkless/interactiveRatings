b1fdae8ae8bea0e64d757044d621767e65467716
Zainab Sansa <tunrayotemitope05@gmail.com>
2023-12-23 22:13:45 +0100

    new commit

--- mine.js
+++ mine.js
@@ -1,2 +1,31 @@
-'use strict';
+document.addEventListener("DOMContentLoaded", function () {
+    const firstSection = document.querySelector(".container");
+    const thankYouSection = document.getElementById("thankYouSection");
+    const overlay = document.querySelector(".overlay");
+
+    
+    function showThankYouSection() {
+        firstSection.style.display = "none";
+        thankYouSection.style.display = "block";
+        overlay.style.display = "block";
+    }
+
+    
+    const submitButton = document.querySelector("button");
+    submitButton.addEventListener("click", showThankYouSection);
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const firstSection = document.querySelector(".container");
+    const thankYouSection = document.getElementById("thankYouSection");
+
+        function showPrevious() {
+        thankYouSection.style.display = "none";
+        previousSection.style.display = "block"; 
+    }
+
+    overlay.addEventListener('click', function() {
+        thankYouSection.id.add('hidden');
+        overlay.classList.add('hidden');
+    }
+)})
 
